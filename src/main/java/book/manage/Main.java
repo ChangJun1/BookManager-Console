aaa8428a0d16c0918c74fb257d3b7c7bd4611edb
aichangjun <190589618@qq.com>
2023-04-15 11:40:50 +0800

    添加Jul日志

--- src/main/java/book/manage/Main.java
+++ src/main/java/book/manage/Main.java
@@ -3,15 +3,22 @@ package book.manage;
 import book.manage.entity.Book;
 import book.manage.entity.Student;
 import book.manage.sql.SqlUtil;
+import lombok.extern.java.Log;
+import org.apache.ibatis.io.Resources;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.LogManager;
 
 /**
  * @author ach
  */
+@Log
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (Scanner scanner = new Scanner(System.in)) {
+            LogManager manager = LogManager.getLogManager();
+            manager.readConfiguration(Resources.getResourceAsStream("logging.properties"));
             while (true) {
                 System.out.println("===================");
                 System.out.println("1. 录入学生信息");
@@ -55,8 +62,10 @@ public class Main {
             int i = mapper.addStudent(student);
             if (i > 0) {
                 System.out.println("学生信息录入成功！");
+                log.info("学生信息录入成功：" + student);
             } else {
                 System.out.println("学生信息录入失败，请重试！");
+                log.info("学生信息录入失败");
             }
         });
     }
@@ -74,8 +83,10 @@ public class Main {
             int i = mapper.addBook(book);
             if (i > 0) {
                 System.out.println("书籍信息录入成功！");
+                log.info("书籍信息录入成功" + book);
             } else {
                 System.out.println("书籍信息录入失败，请重试！");
+                log.info("书籍信息录入失败");
             }
         });
     }
